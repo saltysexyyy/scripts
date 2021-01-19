85e391a0ee9457f4c95c3685c844341c921591b0
saltysexyyy <65953426+saltysexyyy@users.noreply.github.com>
2021-01-20 01:20:47 +0530

    Update tes.ps1

--- tes.ps1
+++ tes.ps1
@@ -1,1 +1,3 @@
-iwr -Uri https://miro.medium.com/max/3000/1*-SVS17G1ff_JZ7Y-kHdcqQ.jpeg -OutFile c:\windows\temp\wallpapaer.jpg;sp 'HKCU:Control Panel\Desktop' WallPaper 'c:\windows\temp\wallpapaer.jpg'; ;$a=1;do{RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;sleep 1}while($a++-le59)"
+iwr -Uri https://miro.medium.com/max/3000/1*-SVS17G1ff_JZ7Y-kHdcqQ.jpeg -OutFile c:\windows\temp\wallpapaer.jpg;
+sp 'HKCU:Control Panel\Desktop' WallPaper 'c:\windows\temp\wallpapaer.jpg';
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;
